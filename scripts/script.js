ed3d9f09e6e44cad852f834dfe6e3ee5281ddd32
JVPH <gyrospizzeria@protonmail.com>
2022-09-24 17:38:04 -0500

    refactor: change logic to use classes(Book and Library)

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,35 @@
-let myLibrary = [];
+class Book {
+    constructor(title, author, nOfPages, read) {
+        this.title = title;
+        this.author = author;
+        this.nOfPages = nOfPages;
+        this.read = read;    
+    }
+}
+class Library {
+    constructor(){
+        this.library = [];
+    }
 
-function Book(title, author, nOfPages, read) {
-    this.title = title;
-    this.author = author;
-    this.nOfPages = nOfPages;
-    this.read = read;    
+    addBook(book){
+        this.library.push(book);
+    }
+
+    getBooks(){
+        console.log(this.library);
+    }    
 }
 
-function addBookToLibrary() {
-    const title = document.querySelector('#title');
-    const author = document.querySelector('#author');
-    const nOfPages = document.querySelector('#nOfPages');
-    const read = document.querySelector('#read');
-    const book = new Book(title.value, author.value, nOfPages.value, read.checked);  
-    myLibrary.push(book);
-}
-
+const library = new Library();
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+const nOfPages = document.querySelector('#nOfPages');
+const read = document.querySelector('#read');
 const form = document.querySelector('#form');
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();    
-    addBookToLibrary();    
+    const book = new Book(title.value, author.value, nOfPages.value, read.checked);  
+    library.addBook(book);    
+    
 });
