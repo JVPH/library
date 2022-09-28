76865ee64d3c89f713126b964e12a5e467e7745f
JVPH <gyrospizzeria@protonmail.com>
2022-09-28 17:19:22 -0500

    fix: change function to avoid books displaying twice

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+const booksDisplay = document.querySelector('.books-display');
 class Book {
     constructor(title, author, nOfPages, read) {
         this.title = title;
@@ -5,19 +6,34 @@ class Book {
         this.nOfPages = nOfPages;
         this.read = read;    
     }
+
+    toString() {        
+        const read = this.read ? 'Yes' : 'No';
+        return `${this.title}\nBy ${this.author}\n${this.nOfPages} pages\nRead? ${read}`
+    }
 }
 class Library {
     constructor(){
-        this.library = [];
+        this.books = [];
     }
 
-    addBook(book){
-        this.library.push(book);
+    addBookToArray(book){
+        this.books = this.books.concat(book);
     }
 
     getBooks(){
-        console.log(this.library);
-    }    
+        console.log(this.books);
+    }  
+    
+}
+
+const displayBooksFromArray = (book) => {    
+    const bookElement = document.createElement('div');
+    bookElement.classList.add('book-element');    
+    const p = document.createElement('p');
+    p.textContent = book.toString();
+    bookElement.appendChild(p);
+    booksDisplay.appendChild(bookElement);          
 }
 
 const library = new Library();
@@ -27,9 +43,11 @@ const nOfPages = document.querySelector('#nOfPages');
 const read = document.querySelector('#read');
 const form = document.querySelector('#form');
 
+
 form.addEventListener('submit', (e) => {
     e.preventDefault();    
     const book = new Book(title.value, author.value, nOfPages.value, read.checked);  
-    library.addBook(book);    
-    
+    library.addBookToArray(book);     
+    form.reset();
+    displayBooksFromArray(book);
 });
